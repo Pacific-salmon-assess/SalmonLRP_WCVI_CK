fa8d243bee80a1ef5c5d366f481b6498f7d28680
lukewarkentin <luke.warkentin@gmail.com>
2020-12-01 16:33:32 -0800

    Moved data prep, infilling, plots to a new r file: prepare_data.R

--- SCChumStudy/make_map.R
+++ SCChumStudy/make_map.R
@@ -1,13 +1,16 @@
+# Make a map of the 7 Inside South Coast Chum Conservation Units
+
 library(bcmaps)
 library(bcmapsdata)
 library(ggplot2)
 library(sf)
+#install.packages('bcmapsdata', repos='https://bcgov.github.io/drat/', lib="C:/R/R-4.0.3/library" ) # only have to run once
 
 # Download chum CU data shapefile from:
 # https://open.canada.ca/data/en/dataset/f86c0867-d38d-4072-bd08-57cbbcbafa46
 
-bc <- bc_bound() # get 
-rivers <- watercourses_5M
+bc <- bc_bound() # get BC province boundary
+rivers <- watercourses_5M # get rivers layer
 chum_cu <- st_read("spatial/Chum Salmon CU Boundary_En.shp")
 # get south coast chum CUs 
 sc_cus <- c("Southern Coastal Streams", "Northeast Vancouver Island", "Upper Knight", "Loughborough", "Bute Inlet", "Georgia Strait", "Howe Sound-Burrard Inlet")
@@ -16,7 +19,7 @@ scc <- chum_cu[chum_cu$CU_name %in% sc_cus, ] # get sf object of just south coas
 # get bounds 
 bounds <- as.numeric(st_bbox(scc))
 # plot
-png("figures/fig_cu_map.png", width=6, height=4, units="in", res=300)
+png("Figures/fig_CU_map.png", width=6, height=4, units="in", res=300)
 ggplot(data = scc) + 
   geom_sf(data=bc) +
   geom_sf(data=scc, aes(fill=CU_name)) +
