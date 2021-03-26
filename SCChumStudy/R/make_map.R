0aee446807e6cd99b53ddeaaee5cab4a0fecf7c2
lukewarkentin <luke.warkentin@gmail.com>
2021-03-26 15:48:39 -0700

    Make chum map pretty

--- SCChumStudy/R/make_map.R
+++ SCChumStudy/R/make_map.R
@@ -4,6 +4,7 @@ library(bcmaps)
 library(ggplot2)
 library(sf)
 library(stars)
+library(ggspatial)
 #install.packages('bcmapsdata', repos='https://bcgov.github.io/drat/', lib="C:/R/R-4.0.3/library" ) # only have to run once
 
 # Download chum CU data shapefile from:
@@ -26,16 +27,28 @@ nb <- bc_neighbours()
 bounds <- as.numeric(st_bbox(scc))
 
 # plot
-png("Figures/fig_chum_CU_map.png", width=7, height=7, units="in", res=300)
+png("Figures/fig_chum_CU_map.png", width=8, height=7, units="in", res=300)
 ggplot(data = scc) + 
-  geom_sf(data=bc, fill="olivedrab") +
-  geom_sf(data=nb[nb$name=="Washington",], fill="olivedrab") +
+  geom_sf(data=bc, fill="antiquewhite") +
+  geom_sf(data=nb[nb$name=="Washington",], fill="antiquewhite") +
   geom_sf(data=scc, aes(fill=CU_name)) +
-  geom_sf_label(data=scc, aes(label = CU_name, colour=CU_name), size=5) +
   #geom_sf(data=riv5, colour="dodgerblue") +
+  geom_sf_label(data=scc, aes(label = CU_name, colour=CU_name), size=3) +
+  annotate( geom="text", label = "British Columbia", x = -121.8, y = 49.5, fontface = "italic", color = "grey22", size = 4) +
+  annotate( geom="text", label = "Washington", x = -121.8, y = 48.5, fontface = "italic", color = "grey22", size = 4) +
+  annotate( geom="text", label = "Pacific Ocean", x = -127.7, y = 49.3, fontface = "italic", color = "darkblue", size = 4) +
+  annotate( geom="text", label = "Salish Sea", x = -123.5, y = 49.2, fontface = "italic", color = "darkblue", size = 3, angle=320) +
   scale_fill_discrete(guide=NULL) +
   scale_colour_discrete(guide=NULL) +
-  coord_sf(xlim = bounds[c(1,3)], ylim = bounds[c(2,4)]) +
-  theme_void() +
-  theme(plot.background = element_rect(fill="paleturquoise1"))
+  coord_sf(xlim = bounds[c(1,3)] + c(0,1) , ylim = bounds[c(2,4)]) +
+  scale_x_continuous(breaks=seq(-128,-122,1)) +
+  xlab(NULL) +
+  ylab(NULL) +
+  annotation_scale(location = "bl", width_hint = 0.5) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+                         style = north_arrow_fancy_orienteering, width=unit(0.3, "in")) +
+  theme_classic() +
+  theme(panel.background = element_rect(fill="aliceblue", colour="black"))
 dev.off()
+
