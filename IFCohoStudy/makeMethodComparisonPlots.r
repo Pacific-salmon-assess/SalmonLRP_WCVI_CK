a047bf19dedcad871d1aa3fb426537f0fad9b97c
Kendra Holt <kendra.holt@dfo-mpo.gc.ca>
2021-12-09 16:19:41 -0800

    Changed escapement data to Natural.Returns for plotMethodsComparisson plot for coho

--- IFCohoStudy/makeMethodComparisonPlots.r
+++ IFCohoStudy/makeMethodComparisonPlots.r
@@ -36,21 +36,32 @@ sourceAll()
 # =====================================================================
 setwd(cohoDir)
 
-CoEscpDat <- read.csv("DataIn/IFCoho_escpByCU.csv")
-# Change header names to match generic data headers (this will allow generic functions from Functions.r to be used)
-colnames(CoEscpDat)[colnames(CoEscpDat)=="CU_ID"] <- "CU"
-colnames(CoEscpDat)[colnames(CoEscpDat)=="MU_Name"] <- "MU"
-colnames(CoEscpDat)[colnames(CoEscpDat)=="ReturnYear"] <- "yr"
-colnames(CoEscpDat)[colnames(CoEscpDat)=="Escapement"] <- "Escp"
+# CoEscpDat <- read.csv("DataIn/IFCoho_escpByCU.csv")
+# # Change header names to match generic data headers (this will allow generic functions from Functions.r to be used)
+# colnames(CoEscpDat)[colnames(CoEscpDat)=="CU_ID"] <- "CU"
+# colnames(CoEscpDat)[colnames(CoEscpDat)=="MU_Name"] <- "MU"
+# colnames(CoEscpDat)[colnames(CoEscpDat)=="ReturnYear"] <- "yr"
+# colnames(CoEscpDat)[colnames(CoEscpDat)=="Escapement"] <- "Escp"
 
 CoSRDat <- read.csv("DataIn/IFCoho_SRbyCU.csv")
 
 CoEscpDat_bySubpop<-read.csv("DataIn/IFCoho_escpBySubpop.csv")
 
 # Change column names to yr, CU_Name, Escp, Subpop_Name, CU_ID
-CoEscpDat_bySubpop<-CoEscpDat_bySubpop %>% select(yr=Return.Year, CU_Name=Conservation.Unit, Escp=Natural.Returns, Subpop_Name=Sub.Population)
+CoEscpDat_bySubpop<-CoEscpDat_bySubpop %>% select(MU_Name=MU_Name, yr=Return.Year, CU_Name=Conservation.Unit, Escp=Natural.Returns, Subpop_Name=Sub.Population)
 tmp.df<-data.frame(CU_Name=unique(CoEscpDat_bySubpop$CU_Name), CU_ID=seq(1,length(unique(CoEscpDat_bySubpop$CU_Name)),by=1))
 CoEscpDat_bySubpop <- left_join(CoEscpDat_bySubpop,tmp.df)
+
+
+# Summary of CU-level escapements based on Natural.Spawners  
+CoEscpDat <- as_tibble(CoEscpDat_bySubpop) %>% group_by(MU_Name, CU_Name, CU_ID, yr) %>% select(MU_Name, CU_Name, CU_ID, yr, Escp) %>%summarize(Escp=sum(Escp))
+CoEscpDat <- as.data.frame(CoEscpDat)
+# Change header names to match generic data headers (this will allow generic functions from Functions.r to be used)
+colnames(CoEscpDat)[colnames(CoEscpDat)=="CU_ID"] <- "CU"
+colnames(CoEscpDat)[colnames(CoEscpDat)=="MU_Name"] <- "MU"
+
+
+
 
 # Restrict data set to years 1998+ based on recommendation from Michael Arbeider
 CoEscpDat <- CoEscpDat %>% filter(yr >= 1998)
